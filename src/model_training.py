75f9f403b83a176be0ee6cd70a56ed491968d63c
vitalij_demjanovic <vitalijdemjanovic@gmail.com>
2025-03-09 01:06:45 +0100

    update motel_training delete comment

--- src/model_training.py
+++ src/model_training.py
@@ -11,25 +11,19 @@ def model_training(df):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Škálovanie dát
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.transform(X_test)
 
-    # Definovanie modelu
     model_gb = GradientBoostingRegressor(n_estimators=150, learning_rate=0.05, max_depth=4,
                                         subsample=0.8, min_samples_split=2, random_state=42)
 
-    # Použitie cross-validation
     cv_scores = cross_val_score(model_gb, X_train_scaled, y_train, cv=5, scoring='neg_mean_absolute_error')
 
-    # Priemer MAE z cross-validation
     print(f"Priemerný MAE (cross-validation): {-cv_scores.mean()}")
 
-    # Trénovanie modelu na celých trénovacích dátach
     model_gb.fit(X_train_scaled, y_train)
 
-    # Testovanie modelu
     y_pred_gb = model_gb.predict(X_test_scaled)
 
     test_mae_gb = mean_absolute_error(y_test, y_pred_gb)
